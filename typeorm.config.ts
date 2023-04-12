5c65d8096eda01f946d3bf721ffbced038dbdd7f
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2023-04-12 23:14:09 +0200

    rewrite ci setup

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -9,11 +9,7 @@ const configService = new ConfigService();
 
 export const ormConfig: DataSourceOptions = {
   type: "postgres",
-  host: configService.get("DB_HOST"),
-  port: configService.get("DB_PORT"),
-  username: configService.get("DB_USER"),
-  password: configService.get("DB_PASSWORD"),
-  database: configService.get("DB_NAME"),
+  url: configService.get("DB_URL"),
   entities: [join(__dirname, "./src/**/*.entity.{ts,js}")],
   migrations: [join(__dirname, "./migrations/**/*.{ts,js}")],
   synchronize: process.env.NODE_ENV !== "production",
