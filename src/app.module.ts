08ccae72dc1bdf63c1c9b859b53342efa8921ee2
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2023-04-12 19:00:39 +0200

    Setup TypeORM with docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { PostsModule } from '~/posts/posts.module';
+import { DataSource } from 'typeorm';
+import { ormConfig } from "../typeorm.config";
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(ormConfig),
+    PostsModule,
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreatePostDto } from '~/posts/dto/create-post.dto';
+import { PostsService } from '~/posts/posts.service';
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  findAll() {
+    return this.postsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(id: number) {
+    return this.postsService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createPostDto: CreatePostDto) {
+    return this.postsService.create(createPostDto);
+  }
+}
