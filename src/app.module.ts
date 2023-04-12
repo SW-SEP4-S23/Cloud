bf98f0419f96633732cb1847143977702095a454
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2023-04-12 20:19:56 +0200

    Remove example files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,11 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
-import { PostsModule } from "~/posts/posts.module";
 import { DataSource } from "typeorm";
 import { ormConfig } from "../typeorm.config";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(ormConfig),
-    PostsModule,
-  ],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(ormConfig)],
 })
 export class AppModule {
   constructor(private dataSource: DataSource) {}
--- src/posts/dto/create-post.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsString, Length } from "class-validator";
-
-export class CreatePostDto {
-  @IsString()
-  @Length(10, 30)
-  title: string;
-}
