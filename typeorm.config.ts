ab6da7dec0d647172607b66ff05a5556192d8d61
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2023-04-12 20:03:47 +0200

    Enable TypeORM synchronization in development mode

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -2,11 +2,11 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
 import { join } from 'path';
- 
+
 config();
- 
+
 const configService = new ConfigService();
- 
+
 export const ormConfig: DataSourceOptions = {
   type: 'postgres',
   host: configService.get('DB_HOST'),
@@ -15,8 +15,8 @@ export const ormConfig: DataSourceOptions = {
   password: configService.get('DB_PASSWORD'),
   database: configService.get('DB_NAME'),
   entities: [join(__dirname, './src/**/*.entity.{ts,js}')],
-  migrations:  [join(__dirname, './migrations/**/*.{ts,js}')],
-  synchronize: process.env.NODE_ENV === "development"
-}
+  migrations: [join(__dirname, './migrations/**/*.{ts,js}')],
+  synchronize: process.env.NODE_ENV === 'development',
+};
 
 export default new DataSource(ormConfig);
