7140068d90b8bffabe4011359c16b84946c8a852
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2023-04-26 15:36:14 +0200

    Migrate to using Prisma setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,8 @@
 import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
-import { ormConfig } from "../typeorm.config";
 import { ExampleModule } from "./example/example.module";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(ormConfig),
-    ExampleModule,
-  ],
+  imports: [ConfigModule.forRoot(), ExampleModule],
 })
 export class AppModule {}
