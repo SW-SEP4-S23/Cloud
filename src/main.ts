8d451e9a201f03ab646f480f882b09bb132a3494
ymersod <simon.99@live.dk>
2023-04-27 19:01:24 +0200

    delete unused import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
-import { WebsocketService } from "./websocket/websocket.service";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
