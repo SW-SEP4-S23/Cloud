8fe18c760b397c08df79530e9b7f2f84ba09b72b
ymersod <simon.99@live.dk>
2023-04-27 17:17:54 +0200

    Create datapoint repository with create datapoint

---
+++ src/websocket/datapoint.repository.ts
@@ -0,0 +1,20 @@
+import { PrismaService } from "../prisma.service";
+import { Datapoint } from "@prisma/client";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class DatapointRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async createDatapoint(params: {
+    timestamp: Date;
+    co2: number;
+    humidity: number;
+    temperature: number;
+  }): Promise<void> {
+    const { timestamp, co2, humidity, temperature } = params;
+    this.prisma.datapoint.create({
+      data: { timestamp, co2, humidity, temperature },
+    });
+  }
+}
