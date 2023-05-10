0d297d854547e82f076bddfdbc3a2455895fc236
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2023-05-10 12:02:15 +0200

    Add comments to the data fields

--- src/websocket/dto/uplink-data.ts
+++ src/websocket/dto/uplink-data.ts
@@ -8,28 +8,28 @@ import {
 
 export class UplinkData {
   @Contains("rx")
-  cmd: string;
+  cmd: string; // identifies type of message, rx = uplink message
 
   @IsString()
-  EUI: string;
+  EUI: string; // device EUI, 16 hex digits (without dashes)
 
   @IsNumber()
-  ts: number;
+  ts: number; // server timestamp as number (seconds from Linux epoch)
 
   @IsBoolean()
-  ack: boolean;
+  ack: boolean; // acknowledgement flag as set by device
 
   @IsNumber()
-  fcnt: number;
+  fcnt: number; // frame counter, a 32-bit number
 
   @IsNumber()
-  port: number;
+  port: number; // port as sent by the end device
 
   @IsOptional()
   @IsString()
-  encdata?: string;
+  encdata?: string; // data payload (APPSKEY encrypted hex string)
 
   @IsOptional()
   @IsString()
-  data?: string;
+  data?: string; // data payload (decrypted, plaintext hex string)
 }
