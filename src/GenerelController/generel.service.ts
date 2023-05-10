ceb406e9f915a2632904c88c8e9d2e1003813722
Ymersod <Simon.99@live.dk>
2023-05-10 13:22:49 +0200

    allVariablesNewValsDTO created

--- src/GenerelController/generel.service.ts
+++ src/GenerelController/generel.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { IntervalQuery } from "../shared/interval-query";
 import { GenerelRepository } from "./generel.repository";
+import { NewValsDTO } from "../shared/newValsDTO";
 
 @Injectable()
 export class GenerelService {
@@ -8,5 +9,9 @@ export class GenerelService {
 
   findAllInterval(interval: IntervalQuery) {
     return this.generelRepository.findAllInterval(interval);
+  }
+
+  setNewValues(newVals: NewValsDTO) {
+    return this.generelRepository.setNewValues(newVals);
   }
 }
