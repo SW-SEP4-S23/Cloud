5dbf0e5a34cddf64fbb87faa09dc6db95a39ecf4
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2023-05-10 13:19:53 +0200

    Validate incoming data packet

--- src/websocket/websocket.service.ts
+++ src/websocket/websocket.service.ts
@@ -5,6 +5,7 @@ import { translateHex } from "./translate-hex";
 import { WebSocket } from "ws";
 import { plainToClass } from "class-transformer";
 import { UplinkData } from "./dto/uplink-data";
+import { validateSync } from "class-validator";
 
 @Injectable()
 export class WebSocketService implements OnModuleInit, OnModuleDestroy {
@@ -27,22 +28,40 @@ export class WebSocketService implements OnModuleInit, OnModuleDestroy {
     this.#socket.on("message", (buffer: Buffer) => {
       const data = JSON.parse(buffer.toString());
 
-      if (data?.cmd === "rx") {
+      // Only handle the data packet of the uplink.
+      if (data?.cmd === "rx" && data?.port == 1) {
+        // Validate incoming data.
         const uplinkData = plainToClass(UplinkData, data);
-        this.onUplinkData(data);
+        const validationErrors = validateSync(uplinkData);
+        if (validationErrors.length > 0) {
+          console.error("Validation of uplink data failed: ", validationErrors);
+          return;
+        }
+
+        this.onUplinkData(uplinkData);
+        return;
       }
+
+      console.info("Messsage wasn't handled: ", data);
     });
   }
 
-  async onUplinkData(data: any) {
-    const hexTranslatedData = translateHex(data.data);
+  async onUplinkData(uplinkData: UplinkData) {
+    if (!uplinkData.data) {
+      console.error("No data in uplink data");
+      return;
+    }
+
+    console.info("Uplink data: ", uplinkData.data);
+
+    const [temperature, co2, humidity] = translateHex(uplinkData.data);
+    const timestamp = new Date(uplinkData.ts);
 
     return this.dpRep.createDatapoint({
-      // Check if data already contains a timestamp
-      timestamp: new Date(),
-      temperature: hexTranslatedData[0],
-      co2: hexTranslatedData[1],
-      humidity: hexTranslatedData[2],
+      timestamp,
+      temperature,
+      co2,
+      humidity,
     });
   }
 }
