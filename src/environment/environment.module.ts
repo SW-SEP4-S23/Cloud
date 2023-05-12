16ae8b6c82e2c00b2fe644528a496b3b1e15526f
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2023-05-13 00:05:30 +0200

    Fix prisma issue and update readme

--- src/environment/environment.module.ts
+++ src/environment/environment.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common";
 import { EnvironmentService } from "./environment.service";
 import { EnvironmentRepository } from "./environment.repository";
-import { PrismaService } from "../prisma.service";
 import { EnvironmentController } from "./environment.controller";
 
 @Module({
-  providers: [EnvironmentService, EnvironmentRepository, PrismaService],
+  providers: [EnvironmentService, EnvironmentRepository],
   controllers: [EnvironmentController],
 })
 export class EnvironmentModule {}
