f4fccae362352f0ca608be19d03b27b39db62b57
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2023-05-18 10:43:13 +0200

    Remove example module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { ExampleModule } from "./example/example.module";
 import { TemperatureModule } from "./temperature/temperature.module";
 import { HumidityModule } from "./humidity/humidity.module";
 import { Co2Module } from "./co2/co2.module";
