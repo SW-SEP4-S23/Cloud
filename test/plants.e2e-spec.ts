c0bb3c51ec1a71f2d39135957db5a3d2e248478e
Milo <miloamadsen@gmail.com>
2023-05-23 09:51:48 +0200

    Fixed e2e to properly match data according to the return values of plants

--- test/plants.e2e-spec.ts
+++ test/plants.e2e-spec.ts
@@ -2,6 +2,7 @@ import { INestApplication } from "@nestjs/common";
 import { AppModule } from "../src/app.module";
 import { Test } from "@nestjs/testing";
 import * as request from "supertest";
+import { plant } from "../prisma/test-data";
 
 describe("Plants Controller", () => {
   let app: INestApplication;
@@ -15,30 +16,82 @@ describe("Plants Controller", () => {
     await app.init();
   });
 
-  //To see the seeded data, find the file in ../prisma/TODO
+  //To see the seeded data, find the file in ../prisma/seed.ts
 
   describe("(GET) Plants", () => {
-    test("Get all plants", () => {
-      return request(app.getHttpServer())
+    //Expected data is created from the testData inside of ../prisma/seed.ts,
+    //it has to be specially created since the return values of plants carry data from other tables within the databse
+    test("Get all plants", async () => {
+      const expectedResponse = [
+        {
+          id: 1,
+          pb_Id: 1,
+          pb: expect.objectContaining({
+            plantingDate: "2020-12-01T00:00:00.000Z",
+            harvestDate: "2021-01-01T00:00:00.000Z",
+            ps: expect.any(Object),
+          }),
+          PlantLogs: expect.arrayContaining([expect.any(Object)]),
+        },
+        {
+          id: 2,
+          pb_Id: 1,
+          pb: expect.objectContaining({
+            plantingDate: "2020-12-01T00:00:00.000Z",
+            harvestDate: "2021-01-01T00:00:00.000Z",
+            ps: expect.any(Object),
+          }),
+          PlantLogs: expect.arrayContaining([expect.any(Object)]),
+        },
+        {
+          id: 3,
+          pb_Id: 2,
+          pb: expect.objectContaining({
+            plantingDate: "2020-12-01T00:00:00.000Z",
+            harvestDate: "2021-01-01T00:00:00.000Z",
+            ps: expect.any(Object),
+          }),
+          PlantLogs: [],
+        },
+      ];
+
+      const response = await request(app.getHttpServer())
         .get("/stock/plants")
-        .expect(200)
-        .expect({});
+        .expect(200);
+
+      expect(response.body).toEqual(expectedResponse);
     });
 
     test("Get plant by id", () => {
       return request(app.getHttpServer())
         .get("/stock/plants/1")
         .expect(200)
-        .expect({});
+        .expect({
+          id: 1,
+          pb_Id: 1,
+          pb: {
+            plantingDate: "2020-12-01T00:00:00.000Z",
+            harvestDate: "2021-01-01T00:00:00.000Z",
+            ps: { name: "Aloe Vera" },
+          },
+          PlantLogs: [
+            {
+              id: 1,
+              timestamp: "2021-01-01T00:00:00.000Z",
+              message: "Plant 1 is doing well",
+              p_Id: 1,
+            },
+          ],
+        });
     });
   });
 
   describe("Error handling", () => {
     test("Get plant with non-existant", () => {
-      return request(app.getHttpServer())
-        .get("/stock/plants/100")
-        .expect(404)
-        .expect({});
+      return request(app.getHttpServer()).get("/stock/plants/100").expect({
+        statusCode: 404,
+        message: "Plant not found",
+      });
     });
   });
 
