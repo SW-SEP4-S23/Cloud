00ff14ec6a0852186855479f8bd60e15e83c17b0
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2023-05-24 10:40:50 +0200

    use loops in tests

--- src/websocket/constants.ts
+++ src/websocket/constants.ts
@@ -1,4 +1,4 @@
 export const IOT_EUI = "0004A30B00F398F2";
-export const MAX_ACK_ID = 255;
+export const MAX_ACK_ID = 254;
 export const LORAWAN_WSS_URL =
   "wss://iotnet.teracom.dk/app?token=vnoVQAAAABFpb3RuZXQudGVyYWNvbS5ka0AHfDGv873AtxYtbA-B0Sw=";
