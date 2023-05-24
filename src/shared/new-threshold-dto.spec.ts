32bda17fdcc953cbf0dbd13d0604bebb9b57370f
Milo <miloamadsen@gmail.com>
2023-05-24 10:32:44 +0200

    Added unit testing for hardcoded thresholds

--- src/shared/new-threshold-dto.spec.ts
+++ src/shared/new-threshold-dto.spec.ts
@@ -1,7 +1,12 @@
 import { Test, TestingModule } from "@nestjs/testing";
-import { NewThresholdDTO, newThresholdChecker } from "./new-threshold-dto";
+import {
+  NewThresholdDTO,
+  newThresholdChecker,
+  testForHardcodedThresholdsCo2,
+  testForHardcodedThresholdsHumidity,
+} from "./new-threshold-dto";
 
-describe("NewValsDTO", () => {
+describe("NewThresholdDTO Unit Tests", () => {
   let provider: NewThresholdDTO;
 
   beforeEach(async () => {
@@ -26,5 +31,59 @@ describe("NewValsDTO", () => {
     provider.minValue = 5;
     provider.maxValue = 10;
     expect(() => newThresholdChecker(provider)).not.toThrowError();
+  });
+
+  describe("Hardcoded Thresholds", () => {
+    describe("Exceptions", () => {
+      describe("Co2", () => {
+        it("Co2 should throw an error if minValue is < hardcoded min", () => {
+          provider.minValue = -1;
+          provider.maxValue = 100;
+          expect(() => testForHardcodedThresholdsCo2(provider)).toThrowError();
+        });
+
+        it("Co2 should throw an error if maxValue is > hardcoded max", () => {
+          provider.minValue = 0;
+          provider.maxValue = 1;
+          expect(() => testForHardcodedThresholdsCo2(provider)).toThrowError();
+        });
+      });
+
+      describe("Humidity", () => {
+        it("Humidity should throw an error if minValue is < hardcoded min", () => {
+          provider.minValue = -1;
+          provider.maxValue = 100;
+          expect(() =>
+            testForHardcodedThresholdsHumidity(provider),
+          ).toThrowError();
+        });
+
+        it("Humidity should throw an error if maxValue is > hardcoded max", () => {
+          provider.minValue = 0;
+          provider.maxValue = 101;
+          expect(() =>
+            testForHardcodedThresholdsHumidity(provider),
+          ).toThrowError();
+        });
+      });
+
+      describe("Temperature", () => {
+        it("Temperature should throw an error if minValue is < hardcoded min", () => {
+          provider.minValue = -41;
+          provider.maxValue = 100;
+          expect(() =>
+            testForHardcodedThresholdsHumidity(provider),
+          ).toThrowError();
+        });
+
+        it("Temperature should throw an error if maxValue is > hardcoded max", () => {
+          provider.minValue = 0;
+          provider.maxValue = 126;
+          expect(() =>
+            testForHardcodedThresholdsHumidity(provider),
+          ).toThrowError();
+        });
+      });
+    });
   });
 });
