a373e65e1b08f34bc169ef2d3863ee130e3a54bb
Simon Due Kristensen <95355670+ymersod@users.noreply.github.com>
2023-05-30 12:14:08 +0200

    Update src/stock/logs/dto/create-species-log-dto.ts
    Co-authored-by: Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>

--- src/stock/logs/dto/create-species-log-dto.ts
+++ src/stock/logs/dto/create-species-log-dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsString } from "class-validator";
 
-export class createSpeciesLogDto {
+export class CreateSpeciesLogDto {
   @IsString()
   @ApiProperty()
   message: string;
